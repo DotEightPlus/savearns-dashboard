40bfc3bc9657897a6ee2ee9ead97e76a96fab347
Abolade Greatness <49532326+Grtnxhor@users.noreply.github.com>
2021-10-01 03:44:08 +0100

    reusauable components

---
+++ include/head.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <title>Savearns | Dashboard</title>
+
+
+    <!-- OG Meta Tags to improve the way the post looks when you share the page on Facebook, Twitter, LinkedIn -->
+    <meta name="description" content="Savearns | Dashboard - Save More While You Spend">
+    <meta name="keywords" content="Savearns">
+    <meta property="og:title" content="Savearns" />
+    <meta property="og:image" content="log.png" />
+    <meta property="og:url" content="https://savearns.com" />
+    <meta property="og:site_name" content="Savearns" />
+    <meta property="og:description" content="Savearns | Dashboard - Save More While You Spend" />
+    <meta charset="UTF-8">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="theme-color" content="#34459C">
+
+
+    <!-- ===============================================-->
+    <!--    Favicons-->
+    <!-- ===============================================-->
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/log.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/log.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/log.png">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/log.png">
+
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+    <!-- Nucleo Icons -->
+    <link href="assets/css/nucleo-icons.css" rel="stylesheet" />
+    <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
+    <!-- CSS Files -->
+    <link id="pagestyle" href="assets/css/soft-ui-dashboard.css?v=1.0.3" rel="stylesheet" />
+</head>
