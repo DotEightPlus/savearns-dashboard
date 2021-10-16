967157d947f19b146bfd78f8f54c653754128ccc
Abolade Greatness <49532326+Grtnxhor@users.noreply.github.com>
2021-10-16 19:44:09 +0100

    new files

--- app/include/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-    <div class="footer-left">
-        &copy; Savearns <?php echo date("Y")
-                    <div class="bullet"></div>
-                    Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
-                </div>
-                <div class="footer-right"></div>
-            </footer>
