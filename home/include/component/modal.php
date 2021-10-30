291bfe99a233ba8e2b151ba494aadf8eb9648103
Abolade Greatness <49532326+Grtnxhor@users.noreply.github.com>
2021-10-30 10:14:50 +0100

    withdraw form

--- home/include/component/modal.php
+++ home/include/component/modal.php
@@ -95,6 +95,45 @@
     </div>
 </div>
 
+<div class="modal fade" tabindex="-1" role="dialog" id="withdModal">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Withdraw Funds</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="col-12 col-md-12 col-sm-12">
+                    <form method="POST" action="#">
+                        <p>Currently under development</p>
+                        <!--<div class="form-group">
+                            <label>Input Amount</label>
+                            <div class="input-group">
+
+                                <input type="number" name="amount" class="form-control" required>
+                            </div>
+                        </div>-->
+
+
+                        <!--<div class="row">
+                            <div class="form-group mb-2 col-md-6 col-lg-6 col-sm-4">
+                                <button type="submit" class="form-control btn-primary">Fund
+                                    Now</button>
+                            </div>
+                            <div class="form-group mb-0 col-md-6 col-lg-6 col-sm-3">
+                                <button type="button" class="form-control btn-primary">Pay with
+                                    Crypto<sup><small> Beta</small></sup> </button>
+                            </div>
+                        </div>-->
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 <div class="modal fade" tabindex="-1" role="dialog" id="transferModal">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
